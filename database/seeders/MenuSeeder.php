0fc10ce13a36d4f6cb6021a6ef05c0cd26bdfb3d
atss0 <ahmettayyip56@gmail.com>
2025-05-27 06:53:38 +0300

    a

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -12,7 +12,7 @@ class MenuSeeder extends Seeder
 {
     public function run()
     {
-        // Mağaza Oluştur
+        // Mağaza
         $store = Store::create([
             'name' => 'MelonBucks Merkez',
             'address' => 'Çorum / Merkez',
@@ -20,7 +20,7 @@ class MenuSeeder extends Seeder
             'longitude' => 34.962829,
         ]);
 
-        // Masalar (5 adet)
+        // Masalar
         foreach (range(1, 5) as $i) {
             Table::create([
                 'store_id' => $store->id,
@@ -29,39 +29,96 @@ class MenuSeeder extends Seeder
             ]);
         }
 
-        // Menü Kategorileri
+        // Kategoriler
         $drinks = MenuCategory::create(['name' => 'İçecekler']);
-        $foods = MenuCategory::create(['name' => 'Yemekler']);
+        $foods  = MenuCategory::create(['name' => 'Yemekler']);
         $desserts = MenuCategory::create(['name' => 'Tatlılar']);
 
-        // Menü Ürünleri
+        // İçecekler
         MenuItem::create([
             'store_id' => $store->id,
             'category_id' => $drinks->id,
             'name' => 'Buzlu Latte',
-            'description' => 'Soğuk espresso ve süt',
-            'price' => 55.00,
-            'image' => 'menu/latte.jpg',
+            'description' => 'Soğuk espresso, süt ve buz.',
+            'price' => 72.00,
+            'image' => 'https://images.unsplash.com/photo-1587734903853-36796baf1f68', // latte
+            'is_popular' => true,
+        ]);
+
+        MenuItem::create([
+            'store_id' => $store->id,
+            'category_id' => $drinks->id,
+            'name' => 'Filtre Kahve',
+            'description' => 'Taze çekilmiş filtre kahve.',
+            'price' => 48.00,
+            'image' => 'https://images.unsplash.com/photo-1511920170033-f8396924c348',
+        ]);
+
+        MenuItem::create([
+            'store_id' => $store->id,
+            'category_id' => $drinks->id,
+            'name' => 'Portakallı Soğuk Çay',
+            'description' => 'Ev yapımı portakallı buzlu çay.',
+            'price' => 45.00,
+            'image' => 'https://images.unsplash.com/photo-1551024601-bec78aea704b',
+        ]);
+
+        // Yemekler
+        MenuItem::create([
+            'store_id' => $store->id,
+            'category_id' => $foods->id,
+            'name' => 'Tavuk Wrap',
+            'description' => 'Izgara tavuk, marul, domates, özel sos.',
+            'price' => 98.00,
+            'image' => 'https://images.unsplash.com/photo-1601050690185-52f9bafdb7f2',
+        ]);
+
+        MenuItem::create([
+            'store_id' => $store->id,
+            'category_id' => $foods->id,
+            'name' => 'Köfte Tabağı',
+            'description' => 'Ev yapımı köfte, pilav ve salata.',
+            'price' => 115.00,
+            'image' => 'https://images.unsplash.com/photo-1600891964599-f61ba0e24092',
             'is_popular' => true,
         ]);
 
         MenuItem::create([
             'store_id' => $store->id,
             'category_id' => $foods->id,
-            'name' => 'Tavuk Wrap',
-            'description' => 'Izgara tavuk ve sebzeli dürüm',
-            'price' => 85.00,
-            'image' => 'menu/wrap.jpg',
+            'name' => 'Vejetaryen Makarna',
+            'description' => 'Sebzeli penne makarna, fesleğenli sos.',
+            'price' => 92.00,
+            'image' => 'https://images.unsplash.com/photo-1603133872874-684f231aa73a',
+        ]);
+
+        // Tatlılar
+        MenuItem::create([
+            'store_id' => $store->id,
+            'category_id' => $desserts->id,
+            'name' => 'Cheesecake',
+            'description' => 'Frambuaz soslu klasik cheesecake.',
+            'price' => 68.00,
+            'image' => 'https://images.unsplash.com/photo-1621272992881-9feebf8a7b1a',
+            'is_popular' => true,
         ]);
 
         MenuItem::create([
             'store_id' => $store->id,
             'category_id' => $desserts->id,
-            'name' => 'Cheesecake',
-            'description' => 'Frambuaz soslu',
-            'price' => 65.00,
-            'image' => 'menu/cheesecake.jpg',
-            'is_popular' => true,
+            'name' => 'Sufle',
+            'description' => 'İçi akışkan çikolatalı sufle.',
+            'price' => 58.00,
+            'image' => 'https://images.unsplash.com/photo-1586985289688-6d831642b0b0',
+        ]);
+
+        MenuItem::create([
+            'store_id' => $store->id,
+            'category_id' => $desserts->id,
+            'name' => 'Tiramisu',
+            'description' => 'Klasik İtalyan tatlısı, kahve ve mascarpone.',
+            'price' => 72.00,
+            'image' => 'https://images.unsplash.com/photo-1589308078050-9720d3c37e35',
         ]);
     }
 }
